e064b6007bb4c61f6a9d2e91da25a41d765cba60
Dylan <wangbo4020@gmail.com>
2018-08-30 00:46:09 +0800

    optimize project.

--- ei/src/main/java/com/daasuu/ei/EasingInterpolator.java
+++ ei/src/main/java/com/daasuu/ei/EasingInterpolator.java
@@ -1,13 +1,13 @@
 package com.daasuu.ei;
 
-import android.animation.TimeInterpolator;
 import android.support.annotation.NonNull;
+import android.view.animation.Interpolator;
 
 /**
  * The Easing class provides a collection of ease functions. It does not use the standard 4 param
  * ease signature. Instead it uses a single param which indicates the current linear ratio (0 to 1) of the tween.
  */
-public class EasingInterpolator implements TimeInterpolator {
+public class EasingInterpolator implements Interpolator {
 
     private final Ease ease;
 
